5ed5985fd5487bea15aadfa59eb4ad1d47dbfdd0
Takumi Kodama <shartsu.in@gmail.com>
2014-12-30 11:22:45 +0900

    [Other] Clean up

--- test/ruby_marks/pdf2png.rb
+++
@@ -1,7 +0,0 @@
-require 'rubygems'
-require 'RMagick'
-
-img = Magick::ImageList.new("./pdf/5pages.pdf"){self.density = 144}
-img.each_with_index{ |img,i| img.write "#{i.to_s}.png" }
-
-p "successfly finished!"
